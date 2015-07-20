c3563b48723756ad806dbcbcf802890398d6fce4
SEINO Katsuhiro <seinokatsuhiro@me.com>
2015-07-20 15:34:50 +0900

    先頭行を削除するオプションを追加

--- koshu-open-data-converter/koshu-from-csv.hs
+++ koshu-open-data-converter/koshu-from-csv.hs
@@ -21,13 +21,44 @@ import qualified Koshucode.Baala.Core           as K
 import qualified Koshucode.Baala.Type.Vanilla   as K
 
 
+-- --------------------------------------------  Param
+
+data Param = Param
+    { paramOmitFirst  :: Bool
+    , paramHeaders    :: [String]
+    , paramJudgeType  :: JudgeType
+    } deriving (Show, Eq, Ord)
+
+initParam :: [Option] -> [String] -> IO Param
+initParam opts args =
+    do hs <- readHeaders opts
+       return $ Param { paramOmitFirst  = OptOmitFirst `elem` opts
+                      , paramHeaders    = hs
+                      , paramJudgeType  = parseJudgeType args
+                      }
+
+readHeaders :: [Option] -> IO [String]
+readHeaders opts =
+    case K.mapMaybe optHeader opts of
+      []     -> return []
+      files  -> mapM readFile files
+
+parseJudgeType :: [String] -> JudgeType
+parseJudgeType [] = ("CSV", map show $ ints 1)
+parseJudgeType (pat@('/' : _) : _) = error $ "Pattern like term name: " ++ pat
+parseJudgeType (pat : ns) = (pat, map name ns) where
+    name ('/' : n) = n
+    name n         = error $ "Not a term: " ++ n
+
+
 -- --------------------------------------------  Option
 
 data Option
     = OptHelp
     | OptVersion
+    | OptOmitFirst
     | OptHeader String
-      deriving (Show, Eq)
+      deriving (Show, Eq, Ord)
 
 optHeader :: Option -> Maybe String
 optHeader (OptHeader s) = Just s
@@ -35,7 +66,8 @@ optHeader (_)           = Nothing
 
 option :: [Opt.OptDescr Option]
 option =
-    [ Opt.Option "" ["header"]  (Opt.ReqArg OptHeader "FILE") "Header file" ]
+    [ Opt.Option ""  ["header"]     (Opt.ReqArg OptHeader "FILE") "Header file"
+    , Opt.Option "1" ["omit-first"] (Opt.NoArg OptOmitFirst)      "Omit first line" ]
 
 
 -- --------------------------------------------  Main
@@ -46,32 +78,32 @@ type JudgeType = (K.JudgePat, [K.TermName])
 main :: IO ()
 main = do args <- Sys.getArgs
           case Opt.getOpt Opt.Permute option args of
-            (opts, files, []) 
-                -> do hs <- readHeaders opts
-                      interact $ fromCSV hs $ parseJudgeType files
+            (opts, args', []) 
+                -> do param <- initParam opts args'
+                      interact $ fromCSV param
             (_, _, errs) -> error $ show errs
 
-ints :: Int -> [Int]
-ints n = [n ..]
-
-parseJudgeType :: [String] -> (K.JudgePat, [K.TermName])
-parseJudgeType [] = ("CSV", map show $ ints 1)
-parseJudgeType (pat@('/' : _) : _) = error $ "Pattern like term name: " ++ pat
-parseJudgeType (pat : ns) = (pat, map name ns) where
-    name ('/' : n) = n
-    name n         = error $ "Not a term: " ++ n
-
-fromCSV :: [String] -> JudgeType -> K.Map String
-fromCSV hs jt s =
+fromCSV :: Param -> K.Map String
+fromCSV param s =
     case CSV.parseCSV "<stdin>" s of
       Left a    -> error $ show a
-      Right csv -> let js = map textJudge $ number csv
-                   in unlines $ appendHeader hs js
+      Right csv -> let js = map textJudge $ number $ omitFirst param csv
+                   in unlines $ appendHeader (paramHeaders param) js
     where
-      textJudge = K.writeDownJudge K.shortEmpty . judge jt
+      textJudge = K.writeDownJudge K.shortEmpty . judge (paramJudgeType param)
 
       number :: [CSV.Record] -> [NumRecord]
       number = zip (ints 1) . K.omit (== [""])
+
+omitFirst :: Param -> K.Map [CSV.Record]
+omitFirst Param { paramOmitFirst = omit } csv
+    | omit       = tail2 csv
+    | otherwise  = csv
+
+appendHeader :: [String] -> K.Map [String]
+appendHeader hs body = ["** -*- koshu -*-"] ++ hs' ++ [""] ++ body where
+    hs'  = map K.commentLine $ concatMap ls hs
+    ls h = "" : lines h
 
 judge :: JudgeType -> NumRecord -> K.JudgeC
 judge (pat, ns) (num, cs) = K.affirm pat xs where
@@ -80,16 +112,12 @@ judge (pat, ns) (num, cs) = K.affirm pat xs where
     toTerm (n, c)  = (n, K.pText c)
 
 
--- --------------------------------------------  Header
+-- --------------------------------------------  Utility
 
-readHeaders :: [Option] -> IO [String]
-readHeaders opts =
-    case K.mapMaybe optHeader opts of
-      []     -> return []
-      files  -> mapM readFile files
+ints :: Int -> [Int]
+ints n = [n ..]
 
-appendHeader :: [String] -> K.Map [String]
-appendHeader hs body = ["** -*- koshu -*-"] ++ hs' ++ [""] ++ body where
-    hs'  = map K.commentLine $ concatMap ls hs
-    ls h = "" : lines h
+tail2 :: K.Map [a]
+tail2 (_ : xs) = xs
+tail2 []       = []
 
