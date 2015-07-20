b8b371493e96b961c90353802185f632b6fe97dd
SEINO Katsuhiro <seinokatsuhiro@me.com>
2015-07-20 12:46:37 +0900

    ヘッダの挿入オプションを追加

--- koshu-open-data-converter/koshu-from-csv.hs
+++ koshu-open-data-converter/koshu-from-csv.hs
@@ -13,18 +13,43 @@
 
 module Main (main) where
 
-import qualified Text.CSV                       as CSV
+import qualified System.Console.GetOpt          as Opt
 import qualified System.Environment             as Sys
+import qualified Text.CSV                       as CSV
 import qualified Koshucode.Baala.Base           as K
 import qualified Koshucode.Baala.Core           as K
 import qualified Koshucode.Baala.Type.Vanilla   as K
+
+
+-- --------------------------------------------  Option
+
+data Option
+    = OptHelp
+    | OptVersion
+    | OptHeader String
+      deriving (Show, Eq)
+
+optHeader :: Option -> Maybe String
+optHeader (OptHeader s) = Just s
+optHeader (_)           = Nothing
+
+option :: [Opt.OptDescr Option]
+option =
+    [ Opt.Option "" ["header"]  (Opt.ReqArg OptHeader "FILE") "Header file" ]
+
+
+-- --------------------------------------------  Main
 
 type NumRecord = (Int, CSV.Record)
 type JudgeType = (K.JudgePat, [K.TermName])
 
 main :: IO ()
 main = do args <- Sys.getArgs
-          interact $ fromCSV $ parseJudgeType args
+          case Opt.getOpt Opt.Permute option args of
+            (opts, files, []) 
+                -> do hs <- readHeaders opts
+                      interact $ fromCSV hs $ parseJudgeType files
+            (_, _, errs) -> error $ show errs
 
 ints :: Int -> [Int]
 ints n = [n ..]
@@ -36,12 +61,12 @@ parseJudgeType (pat : ns) = (pat, map name ns) where
     name ('/' : n) = n
     name n         = error $ "Not a term: " ++ n
 
-fromCSV :: JudgeType -> K.Map String
-fromCSV jt s =
+fromCSV :: [String] -> JudgeType -> K.Map String
+fromCSV hs jt s =
     case CSV.parseCSV "<stdin>" s of
       Left a    -> error $ show a
       Right csv -> let js = map textJudge $ number csv
-                   in unlines $ "-*- koshu -*-" : js
+                   in unlines $ appendHeader hs js
     where
       textJudge = K.writeDownJudge K.shortEmpty . judge jt
 
@@ -53,4 +78,18 @@ judge (pat, ns) (num, cs) = K.affirm pat xs where
     xs = ("#", K.pInt num) : (map toTerm $ zip ns cs)
     toTerm (n, "") = (n, K.empty)
     toTerm (n, c)  = (n, K.pText c)
+
+
+-- --------------------------------------------  Header
+
+readHeaders :: [Option] -> IO [String]
+readHeaders opts =
+    case K.mapMaybe optHeader opts of
+      []     -> return []
+      files  -> mapM readFile files
+
+appendHeader :: [String] -> K.Map [String]
+appendHeader hs body = ["** -*- koshu -*-"] ++ hs' ++ [""] ++ body where
+    hs'  = map K.commentLine $ concatMap ls hs
+    ls h = "" : lines h
 
