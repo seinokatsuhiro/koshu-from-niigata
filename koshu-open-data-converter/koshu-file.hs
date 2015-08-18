396cc1ef11ec5f9f611653ddd5269a11b46b3bb1
SEINO Katsuhiro <seinokatsuhiro@me.com>
2015-08-19 06:24:27 +0900

    時間帯つきの変更時刻

--- koshu-open-data-converter/koshu-file.hs
+++ koshu-open-data-converter/koshu-file.hs
@@ -11,6 +11,7 @@
 module Main (main) where
 
 import qualified Data.Time.Calendar             as Cal
+import qualified Data.Time.LocalTime            as Time
 import qualified Foreign.C.Types                as Posix
 import qualified System.Posix.Types             as Posix
 import qualified System.PosixCompat.Files       as Posix
@@ -20,38 +21,44 @@ import qualified Koshucode.Baala.Base           as K
 import qualified Koshucode.Baala.Core           as K
 import qualified Koshucode.Baala.Type.Vanilla   as K
 
-main :: IO ()
-main = do putStrLn "-*- koshu -*-"
-          body 0 ["."]
+data Param = Param
+    { paramTimeZone :: Time.TimeZone
+    } deriving (Show, Eq, Ord)
 
-body :: Int -> [FilePath] -> IO ()
-body n paths =
+main :: IO ()
+main = do zone <- Time.getCurrentTimeZone
+          let param = Param { paramTimeZone = zone }
+          putStrLn "-*- koshu -*-"
+          body param 0 ["."]
+
+body :: Param -> Int -> [FilePath] -> IO ()
+body param n paths =
     do paths' <- dirExpand paths
        case paths' of
          p:ps -> do K.when (n `mod` 10 == 0) $ putStrLn ""
-                    j <- judgeIO p
+                    j <- judgeIO param p
                     K.putJudge j
-                    body (n + 1) ps
+                    body param (n + 1) ps
 
          []   -> do putStrLn ""
                     putStrLn $ "*** " ++ show n ++ " judges"
 
-judgeIO :: FilePath -> IO K.JudgeC
-judgeIO path =
+judgeIO :: Param -> FilePath -> IO K.JudgeC
+judgeIO param path =
     do stat <- Posix.getFileStatus path
-       return $ judge path
+       return $ judge param path
                   (Posix.fileSize stat)
                   (Posix.modificationTime stat)
 
-judge :: FilePath -> Posix.COff -> Posix.CTime -> K.JudgeC
-judge path (Posix.COff size) (Posix.CTime time) = K.affirm "FILE" xs
-    where
-      tc = case timeFromUnix time of
-             Left _  -> K.empty
-             Right t -> K.pTime t
-      xs = [ K.term "time" $ tc
-           , K.term "size" $ K.pInteger $ fromIntegral size
-           , K.term "path" $ K.pText path ]
+judge :: Param -> FilePath -> Posix.COff -> Posix.CTime -> K.JudgeC
+judge param path (Posix.COff size) (Posix.CTime time) = K.affirm "FILE" xs where
+    xs = [ K.term "time" $ tc
+         , K.term "size" $ K.pInteger $ fromIntegral size
+         , K.term "path" $ K.pText path ]
+    zone = Just $ 60 * (Time.timeZoneMinutes $ paramTimeZone param)
+    tc = case timeFromUnix time zone of
+           Left _  -> K.empty
+           Right t -> K.pTime t
 
 dirExpand :: [FilePath] -> IO [FilePath]
 dirExpand [] = return []
@@ -70,12 +77,12 @@ dirFiles path =
        let ns' = K.omit (`elem` [".", ".."]) ns
        return ns'
 
-timeFromUnix :: (Integral n) => n -> K.Ab K.Time
-timeFromUnix time =
+timeFromUnix :: (Integral n) => n -> Maybe K.Sec -> K.Ab K.Time
+timeFromUnix time zone =
     case K.dhmsFromSec $ fromIntegral time of
       (day, h, m, s) -> let date  = K.Monthly $ dayFromUnix day
                             clock = K.clockFromHms h m (Just s)
-                        in K.timeFromDczAb date clock Nothing
+                        in K.timeFromDczAb date clock zone
 
 dayFromUnix :: (Integral n) => n -> Cal.Day
 dayFromUnix day = Cal.ModifiedJulianDay $ fromIntegral day + 40587
